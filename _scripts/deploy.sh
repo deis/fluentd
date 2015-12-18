87fcfc9641669ee0ca347abe8c2f77e053e5f49d
Jonathan Chauncey <jchauncey@deis.com>
2015-12-18 10:10:59 -0700

    fix(deploy.sh): Make deploy.sh like the other deploy scripts

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -9,10 +9,8 @@ export BUILD_TAG=v2-alpha
 echo "Logging into docker hub account!"
 docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
 echo "Building docker image and pushing to docker hub!"
-docker build -t deisci/fluentd:$VERSION ..
-docker push deisci/fluentd:$VERSION
+make -C .. build push
 echo "Logging into quay.io account!"
 docker login -e="$QUAY_EMAIL" -u="$QUAY_USERNAME" -p="$QUAY_PASSWORD" quay.io
 echo "Building docker image and pushing to quay.io!"
-docker build -t quay.io/deisci/fluentd:$VERSION
-docker push quay.io/deisci/fluentd:$VERSION
+DEIS_REGISTRY=quay.io/ make -C .. build push
